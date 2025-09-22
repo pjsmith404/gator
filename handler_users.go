d659cd828a970e7d6ee68ad8adb3d2ad394872f2
pjsmith404 <pjsmith404@gmail.com>
2025-09-22 20:52:22 +1000

    Add a users command

---
+++ handler_users.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"context"
+)
+
+func handlerUsers(s *state, cmd command) error {
+	users, err := s.db.ListUsers(context.Background())
+	if err != nil {
+		return err
+	}
+
+	currentUser := s.config.CurrentUserName
+	
+	for _, user := range users {
+		if user.Name == currentUser {
+			fmt.Println("*", user.Name, "(current)")
+		} else {
+			fmt.Println("*", user.Name)
+		}
+	}
+
+	return nil
+}
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -12,5 +12,8 @@ RETURNING *;
 SELECT * FROM users
 WHERE name = $1 LIMIT 1;
 
+-- name: ListUsers :many
+SELECT * FROM users;
+
 -- name: DeleteUsers :exec
 DELETE FROM users;
