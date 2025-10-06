3618451c0131dc8b4c27e77bed797018e7ce3a75
pjsmith404 <pjsmith404@gmail.com>
2025-10-06 20:52:04 +1100

    Move user functions into handler_users.go

--- handler_users.go
+++ handler_users.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/google/uuid"
+	"github.com/pjsmith404/gator/internal/database"
+	"time"
 )
 
 func handlerUsers(s *state, cmd command) error {
@@ -20,6 +23,56 @@ func handlerUsers(s *state, cmd command) error {
 			fmt.Println("*", user.Name)
 		}
 	}
+
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("No username provided")
+	}
+
+	user := cmd.args[0]
+	newUser := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+	}
+
+	createdUser, err := s.db.CreateUser(context.Background(), newUser)
+	if err != nil {
+		return err
+	}
+
+	err = s.config.SetUser(user)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("User registered:", createdUser)
+
+	return nil
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("No username provided")
+	}
+
+	name := cmd.args[0]
+
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("User not regstered: %v", err)
+	}
+
+	err = s.config.SetUser(name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("User set to", name)
 
 	return nil
 }
