e28759ceecb2477be0fa6224504b0bbbb7fd4dda
pjsmith404 <pjsmith404@gmail.com>
2025-10-15 20:12:44 +1100

    Add GetFeedFollowsForUser query

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -13,3 +13,9 @@ FROM inserted_feed
 JOIN users on inserted_feed.user_id = users.id
 JOIN feeds on inserted_feed.feed_id = feeds.id;
 
+-- name: GetFeedFollowsForUser :many
+SELECT feed_follows.*, users.name AS user_name, feeds.name AS feed_name
+FROM feed_follows
+JOIN users on inserted_feed.user_id = users.id
+JOIN feeds on inserted_feed.feed_id = feeds.id
+WHERE feed_follows.user_id = $1;
