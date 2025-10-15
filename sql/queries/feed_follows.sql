b1da43e67ae1b14719fcb0f16dce85b7f3e32983
pjsmith404 <pjsmith404@gmail.com>
2025-10-15 19:35:45 +1100

    Add CreateFeedFollow query

---
+++ sql/queries/feed_follows.sql
@@ -0,0 +1,14 @@
+-- name: CreateFeedFollow :one
+WITH inserted_feed AS (
+	INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
+	VALUES (
+		$1,
+		$2,
+		$3,
+		$4,
+		$5
+	) RETURNING *
+) SELECT inserted_feed.*, users.name AS user_name, feeds.name AS feed_name FROM inserted_feed
+	LEFT JOIN users on inserted_feed.user_id = users.id
+	LEFT JOIN feeds on inserted_feed.feed_id = feeds.id;
+
