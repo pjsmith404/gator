d0f0d8c9eca62a54c925853ffee5d3ebc45b2382
pjsmith404 <pjsmith404@gmail.com>
2025-10-30 15:56:30 +1100

    Add posts table

---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+CREATE TABLE posts (
+	id         uuid      PRIMARY KEY,
+	created_at   timestamp NOT NULL,
+	updated_at   timestamp NOT NULL,
+	title        text      NOT NULL,
+	url          text      NOT NULL UNIQUE,
+	description  text,
+	published_at timestamp NOT NULL,
+	feed_id      uuid      NOT NULL REFERENCES feeds(id)
+	                           ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE posts;
+
